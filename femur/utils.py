a24d8ed285fd7df0b37f0f5154bbbdbfd5494bb5
Alex <alex@recordsonribs.com>
2016-01-02 17:04:42 +0000

    Lots of tests and extended functionality

--- femur/utils.py
+++ femur/utils.py
@@ -1,16 +1,46 @@
+import re
+import unicodedata
+
+def strip_accents(text):
+    try:
+        text = unicode(text, 'utf-8')
+    except NameError:
+        pass
+    text = unicodedata.normalize('NFD', text)
+    text = text.encode('ascii', 'ignore')
+    text = text.decode("utf-8")
+    return str(text)
+
 def only_real_files(list):
 	return [x for x in list if not x.startswith('.')]
 
 def clean_format(format):
-	return 'Ogg' if format.endswith('Vorbis') else format
+	return 'ogg' if format.endswith('Vorbis') else format.lower()
+
+def truncate(string):
+	bits = string.split()
+
+	if len(bits) <= 3:
+		return '-'.join(bits)
+
+	return ''.join(word[0] for word in bits)
+
+def remove_special_characters(string):
+	return re.sub('[^A-Za-z0-9 \.]+', '', string)
+
+def clean_up_input(string):
+	string = strip_accents(string)
+	string = remove_special_characters(string)
+	string = truncate(string)
+	return string.lower()
 
 def zip_file_name(artist, title, format):
-	return "-".join(["".join(word[0].lower() for word in artist.split()), title.lower(), clean_format(format).lower()]) + ".zip"
+	return '-'.join([clean_up_input(artist), clean_up_input(title), clean_format(format)]) + '.zip'
 
 def img_file_name(album, size):
-	return "-".join([album.lower(), (str(size) + 'x' + str(size))]) + '.jpg'
+	return '-'.join([album.lower(), (str(size) + 'x' + str(size))]) + '.jpg'
 
 def directory_name(artist, title, format):
 	if format.endswith('Vorbis'):
 		format = 'Ogg'
-	return "%s - %s [%s]" % (artist, title, format)
+	return '%s - %s [%s]' % (artist, title, format)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,15 +1,49 @@
+# coding=utf8
 import femur.utils as utils
 
 class TestCleanFormat:
 
     def test_detects_vobris_and_removes(self):
-        assert utils.clean_format('Ogg Vorbis') == 'Ogg'
+        assert utils.clean_format('Ogg Vorbis') == 'ogg'
 
     def test_passes_through_other_formats(self):
-        assert utils.clean_format('MP3 Magic Time') == 'MP3 Magic Time'
+        assert utils.clean_format('MP3') == 'mp3'
+
+
+class TestStripAccents:
+
+    def test_strips_accents_we_might_find_a_problem(self):
+        assert utils.strip_accents('Les Étoiles über Mère Françoise noël') == 'Les Etoiles uber Mere Francoise noel'
 
 
 class TestZipFileName:
 
-    def test_current_functionality(self):
-        assert utils.zip_file_name('Artist', 'Title', 'FLAC') == 'a-title-flac.zip'
+    def test_format_is_lowercased(self):
+        assert utils.zip_file_name('a', 'b', 'FLAC') == 'a-b-flac.zip'
+
+    def test_single_word_artist_and_album(self):
+        assert utils.zip_file_name('Radiohead', 'Drill', 'MP3') == 'radiohead-drill-mp3.zip'
+
+    def test_multiple_word_artist_under_three_words_is_passed_through(self):
+        assert utils.zip_file_name('The Smashing Pumpkins', 'Machina', 'FLAC') == 'the-smashing-pumpkins-machina-flac.zip'
+
+    def test_artist_with_artist_over_three_words_is_truncated(self):
+        assert utils.zip_file_name('All The Empires Of The World', 'Album', 'MP3') == 'ateotw-album-mp3.zip'
+
+    def test_multiple_word_title_under_three_words_is_passed_through(self):
+        assert utils.zip_file_name('Radiohead', 'OK Computer', 'ogg') == 'radiohead-ok-computer-ogg.zip'
+
+    def test_title_with_three_words_is_truncated(self):
+        assert utils.zip_file_name('Artist', 'One Two Three Four', 'MP3') == 'artist-ottf-mp3.zip'
+
+    def test_artist_with_special_characters_is_stripped(self):
+        assert utils.zip_file_name('Artist!?!?%%%&&&@@@!?', 'Album', 'MP3') == 'artist-album-mp3.zip'
+
+    def test_album_with_special_characters_but_short_is_stripped(self):
+        assert utils.zip_file_name('Artist', 'Some Short Name?!!', 'MP3') == 'artist-some-short-name-mp3.zip'
+
+    def test_artist_with_foreign_characters_is_handled(self):
+        assert utils.zip_file_name('Les Étoiles', 'Album', 'FLAC') == 'les-etoiles-album-flac.zip'
+
+    def test_album_with_foreign_characters_is_handled(self):
+        assert utils.zip_file_name('Normal Name', 'über', 'MP3') == 'normal-name-uber-mp3.zip'
